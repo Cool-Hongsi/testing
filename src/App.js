8dbd9c0e9cbf2ba2236351d2a0aa04a87a5e5724
Sungjun <hongsi140626@gmail.com>
2019-08-15 17:41:51 -0400

    create testingComponent branch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import Header from './component/header/index';
+
 export default class App extends React.Component{
   render(){
     return(
       <div>
-        Test
+        <Header />
       </div>
     )
   }
