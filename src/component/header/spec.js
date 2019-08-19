67641b8d3a96eb01532706025791da67cf73af80
Sungjun <hongsi140626@gmail.com>
2019-08-19 19:05:11 -0400

    aa

--- src/component/header/spec.js
+++ src/component/header/spec.js
@@ -1,20 +1,12 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
-// import Header from './index';
-
-// describe('Header Component', () => {
-    
-//     it('It should render without errors', () => {
-//         const component = shallow(<Header />);
-//         const wrapper = component.find(".headerComponent");
-//         expect(wrapper.legnth).toBe(1);
-//     })
-// })
-
 import React from 'react';
 import { shallow } from 'enzyme';
 import Header from './index';
 
-it('It should render without errors', () => {
-
-});
+describe('Header Component', () => {
+    
+    it('It should render without errors', () => {
+        const component = shallow(<Header />);
+        const wrapper = component.find(".headerComponent");
+        expect(wrapper.legnth).toBe(1);
+    })
+})
